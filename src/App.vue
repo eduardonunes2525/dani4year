00b92e06992493b5dc2eddb8a4955319eff08767
Eduardo Nunes <eduardonunes2525@gmail.com>
2019-02-23 12:49:33 -0300

    Add marks in text

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   	<v-container grid-list-md text-xs-center>
 		<v-layout row wrap>
       		<v-flex xs12>
-				<div data-aos="fade-down" class="headline text-uppercase text-styles" >Oi coisinha,</div>
+				<div data-aos="fade-down" class="headline text-uppercase" style="margin-bottom: 20px;" >Oi coisinha,</div>
       		</v-flex>
       		<v-flex xs6>
 					<div data-aos="fade-down">Completaram se 4 anos que você está ao meu lado, e é interessante notar que esta contagem teve início em uma <b>sala de cinema.</b></div>
@@ -12,40 +12,40 @@
       		<v-flex xs6></v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6>
-					<div data-aos="fade-down">Na qual, houve um momento crítico que poderia impactar toda a realidade que hoje conhecemos,</div>
+					<div data-aos="fade-right">Na qual, houve um momento crítico que poderia impactar toda a realidade que hoje conhecemos,</div>
       		</v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6>
-					<div data-aos="fade-down">e um simples <i>sorrir e acenar</i> não seria suficiente,</div>
+					<div data-aos="fade-left">e um simples <i>sorrir e acenar</i> não seria suficiente,</div>
       		</v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6>
-					<div data-aos="fade-down">algo devia ser feito. E que o seria isto? é claro que você já sabe, mas algo que você talvez não saiba é que não foi um simples beijo.</div>
+					<div data-aos="fade-down-right">algo devia ser feito. E que o seria isto? é claro que você já sabe, mas algo que você talvez não saiba é que não foi um simples beijo.</div>
       		</v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6>
-					<div data-aos="fade-down">Foi o <b>beijo do meu verdadeiro amor</b>,</div>
+					<div data-aos="fade-down-left">Foi o <b>beijo do meu verdadeiro amor</b>,</div>
       		</v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6>
-					<div data-aos="fade-down">e que felizmente achei você antes que a <b>última pétala caísse</b>,</div>
+					<div data-aos="flip-left">e que felizmente achei você antes que a <b>última pétala caísse</b>,</div>
       		</v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6>
-					<div data-aos="fade-down">talvez tive sorte ou quem sabe ganhei uma forcinha dos meus <b>amigos do outro lado</b>.</div>
+					<div data-aos="flip-right">talvez tive sorte ou quem sabe ganhei uma forcinha dos meus <b>amigos do outro lado</b>.</div>
       		</v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs12>
-					<div data-aos="fade-down">Depois deste simples, e ao mesmo tempo extremamente complexo, momento, nossas vidas mudaram drasticamente.</div>
+					<div data-aos="flip-up">Depois deste simples, e ao mesmo tempo extremamente complexo, momento, nossas vidas mudaram drasticamente.</div>
       		</v-flex>
       		<v-flex xs6>
-					<div data-aos="fade-down"><i>Os seus problemas viraram os meus também e passei a fazer coisas que faço por você e mais ninguém.</i></div>
+					<div data-aos="flip-down"><i>Os seus problemas viraram os meus também e passei a fazer coisas que faço por você e mais ninguém.</i></div>
       		</v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6></v-flex>
@@ -55,57 +55,57 @@
       		<v-flex xs6></v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6>
-					<div data-aos="fade-down">mas o mais importante é que continuamos a nadar.</div>
+					<div data-aos="zoom-in">mas o mais importante é que <b>continuamos a nadar.</b></div>
       		</v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6>
-					<div data-aos="fade-down">Durante 1460 dias, adicionamos muitas novas histórias no nosso livro de aventuras e muitas ainda estão por vir.</div>
+					<div data-aos="zoom-out">Durante 1460 dias, adicionamos muitas novas histórias no nosso <b>livro de aventuras</b> e muitas ainda estão por vir.</div>
       		</v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6>
-					<div data-aos="fade-down">Passei a dirigir não mais para ganhar tempo, e sim para aproveitá-lo.</div>
+					<div data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine"><i>Passei a dirigir não mais para ganhar tempo, e sim para aproveitá-lo.</i></div>
       		</v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6>
-					<div data-aos="fade-down">Deixando de parar de sobreviver para começar a viver.</div>
+					<div data-aos="fade-down"><i>Parando de sobreviver para começar a viver.</i></div>
       		</v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6>
-					<div data-aos="fade-down">E cada vez que reflito a respeito é curioso entender, que desde o começo simplesmente sabia que tinha feito uma boa escolha!</div>
+					<div data-aos="fade-up">E cada vez que reflito a respeito é curioso entender, que desde o começo simplesmente <b>sabia que tinha feito uma boa escolha!</b></div>
       		</v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6>
-					<div data-aos="fade-down">E fica cada vez mais surpreso que de 7.53 bilhões de pessoas vivendo na mesma época, foi você a pessoa que estava no lugar certo e na hora certa.</div>
+					<div data-aos="fade-left">E fica cada vez mais surpreso que de <b>7.53 bilhões</b> de pessoas vivendo na mesma época, foi você a pessoa que estava no <b>lugar certo e na hora certa.</b></div>
       		</v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6>
-					<div data-aos="fade-down">E que essas 35040 horas viraram história, as horas que virão são um mistério, mas o hoje é uma dádiva.</div>
+					<div data-aos="fade-right">E que essas 35040 horas viraram história, as horas que virão são um mistério, mas o hoje é uma dádiva. É por isso que se chama presente.</div>
       		</v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6>
-					<div data-aos="fade-down">É por isso que se chama presente. E por isso gostaria de te agradecer profundamente pois, eu nada seria se não fosse você.</div>
+					<div data-aos="fade-up-right">E por isso gostaria de te agradecer profundamente pois, <b>eu nada seria se não fosse você.</b></div>
       		</v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6>
-					<div data-aos="fade-down">Obrigado, por escolher trilhar o seu caminho ao meu lado, saiba que no meu coração você vai sempre estar e para o que você precisar saiba que eu estou aqui seguindo esse ciclo sem fim fazendo somente o necessário, pois o extraordinário é demais.</div>
+					<div data-aos="fade-down">Obrigado, por escolher trilhar o seu caminho ao meu lado, saiba que <b>no meu coração você vai sempre estar</b> e para o que você precisar saiba que <b>eu estou aqui</b> seguindo esse <b>ciclo sem fim</b> fazendo <b>somente o necessário, pois o extraordinário é demais.</b></div>
       		</v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6>
-					<div data-aos="fade-down">Mas tendo a certeza de que você é minha ohana e somente com você sou capaz de ouvir as vozes das montanhas e com as cores do vento colorir. E vamos seguir juntos para o nosso infinito e além.</div>
+					<div data-aos="fade-down-left">Mas tendo a certeza de que você é minha <b>ohana</b> e somente com você sou capaz de <b>ouvir as vozes das montanhas e com as cores do vento colorir</b>. E vamos seguir juntos para o nosso <b>infinito e além.</b></div>
       		</v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs6></v-flex>
       		<v-flex xs12>
-				<h1>Obrigado por tudo minha Dani!</h1>
+				<h1 class="headline text-uppercase">Obrigado por tudo minha Dani!!!</h1>
       		</v-flex>
 
 		</v-layout>
