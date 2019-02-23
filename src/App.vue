1f723b2e5ba9bfd6ea1efcec958fd22b50fa3c70
Eduardo Nunes <eduardonunes2525@gmail.com>
2019-02-23 11:52:15 -0300

    My first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,140 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+	<div data-aos="fade-down"></div>
+  	<v-container grid-list-md text-xs-center>
+		<v-layout row wrap>
+      		<v-flex xs12>
+				<div data-aos="fade-down" class="headline text-uppercase text-styles" >Oi coisinha,</div>
+      		</v-flex>
+      		<v-flex xs6>
+					<div data-aos="fade-down">Completaram se 4 anos que você está ao meu lado, e é interessante notar que esta contagem teve início em uma <b>sala de cinema.</b></div>
+      		</v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6>
+					<div data-aos="fade-down">Na qual, houve um momento crítico que poderia impactar toda a realidade que hoje conhecemos,</div>
+      		</v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6>
+					<div data-aos="fade-down">e um simples <i>sorrir e acenar</i> não seria suficiente,</div>
+      		</v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6>
+					<div data-aos="fade-down">algo devia ser feito. E que o seria isto? é claro que você já sabe, mas algo que você talvez não saiba é que não foi um simples beijo.</div>
+      		</v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6>
+					<div data-aos="fade-down">Foi o <b>beijo do meu verdadeiro amor</b>,</div>
+      		</v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6>
+					<div data-aos="fade-down">e que felizmente achei você antes que a <b>última pétala caísse</b>,</div>
+      		</v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6>
+					<div data-aos="fade-down">talvez tive sorte ou quem sabe ganhei uma forcinha dos meus <b>amigos do outro lado</b>.</div>
+      		</v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs12>
+					<div data-aos="fade-down">Depois deste simples, e ao mesmo tempo extremamente complexo, momento, nossas vidas mudaram drasticamente.</div>
+      		</v-flex>
+      		<v-flex xs6>
+					<div data-aos="fade-down"><i>Os seus problemas viraram os meus também e passei a fazer coisas que faço por você e mais ninguém.</i></div>
+      		</v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6>
+					<div data-aos="fade-down">E quatro anos se passaram e principalmente neste quarto ano, enfrentamos vários problemas e em muitas dessas situações tivemos que apenas improvisar,</div>
+      		</v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6>
+					<div data-aos="fade-down">mas o mais importante é que continuamos a nadar.</div>
+      		</v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6>
+					<div data-aos="fade-down">Durante 1460 dias, adicionamos muitas novas histórias no nosso livro de aventuras e muitas ainda estão por vir.</div>
+      		</v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6>
+					<div data-aos="fade-down">Passei a dirigir não mais para ganhar tempo, e sim para aproveitá-lo.</div>
+      		</v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6>
+					<div data-aos="fade-down">Deixando de parar de sobreviver para começar a viver.</div>
+      		</v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6>
+					<div data-aos="fade-down">E cada vez que reflito a respeito é curioso entender, que desde o começo simplesmente sabia que tinha feito uma boa escolha!</div>
+      		</v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6>
+					<div data-aos="fade-down">E fica cada vez mais surpreso que de 7.53 bilhões de pessoas vivendo na mesma época, foi você a pessoa que estava no lugar certo e na hora certa.</div>
+      		</v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6>
+					<div data-aos="fade-down">E que essas 35040 horas viraram história, as horas que virão são um mistério, mas o hoje é uma dádiva.</div>
+      		</v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6>
+					<div data-aos="fade-down">É por isso que se chama presente. E por isso gostaria de te agradecer profundamente pois, eu nada seria se não fosse você.</div>
+      		</v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6>
+					<div data-aos="fade-down">Obrigado, por escolher trilhar o seu caminho ao meu lado, saiba que no meu coração você vai sempre estar e para o que você precisar saiba que eu estou aqui seguindo esse ciclo sem fim fazendo somente o necessário, pois o extraordinário é demais.</div>
+      		</v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6>
+					<div data-aos="fade-down">Mas tendo a certeza de que você é minha ohana e somente com você sou capaz de ouvir as vozes das montanhas e com as cores do vento colorir. E vamos seguir juntos para o nosso infinito e além.</div>
+      		</v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs6></v-flex>
+      		<v-flex xs12>
+				<h1>Obrigado por tudo minha Dani!</h1>
+      		</v-flex>
+
+		</v-layout>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init();
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      //
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.text-styles {
+	text-align: left;
 }
 </style>
